56b768b3bb06fff54652cae1ddd58489d1c4dd9f
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-12-09 09:24:23 -0800

    user model

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,3 @@
 export const PORT = process.env.PORT || 3000;
+export const MONGODB_URL =
+    process.env.MONGODB_URL || 'mongodb://127.0.0.1/diablocms';
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,32 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import { PORT } from './constants';
-
+import { PORT, MONGODB_URL } from './constants';
+// import * as models from './models';
 import express from 'express';
+import mongoose from 'mongoose';
+
+import router from './routes';
 
 const app = express();
 
-app.get('/', (_, res) => {
-    res.send('Hello World!');
-});
+mongoose.connect(MONGODB_URL);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', router);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
+    // models.User.findOne(
+    //     {
+    //         where: { username: 'admin' },
+    //     },
+    //     { password: 0, __v: 0, createdAt: 0, updatedAt: 0 }
+    // )
+    //     .then((user) => {
+    //         console.log(user);
+    //     })
+    //     .catch((err) => {
+    //         console.log(err);
+    //     });
 });
